4dd95014e597bc3c769b5e1fdf8c3f4868e2dbc2
Noelia Martinez <noeliamtzrojo@gmail.com>
2023-08-05 19:49:54 +0100

    Load ember server configuration from application.conf

--- src/main/scala/com/nmartinez/mpr/Application.scala
+++ src/main/scala/com/nmartinez/mpr/Application.scala
@@ -1,16 +1,25 @@
 package com.nmartinez.mpr
 
-import cats.*
+import cats._
+import cats.implicits._
 import cats.effect.{IO, IOApp}
 import org.http4s.ember.server.EmberServerBuilder
 import org.http4s.HttpRoutes
 import org.http4s.dsl.Http4sDsl
+import pureconfig.ConfigSource
 import com.nmartinez.mpr.http.routes.HealthRoutes
+import com.nmartinez.mpr.config.EmberConfig
+import com.nmartinez.mpr.config.Syntax.loadF
 
 object Application extends IOApp.Simple {
-  override def run = EmberServerBuilder
-    .default[IO]
-    .withHttpApp(HealthRoutes[IO].routes.orNotFound)
-    .build
-    .use(_ => IO.println("Server ready!") *> IO.never)
+
+  override def run = ConfigSource.default.loadF[IO, EmberConfig].flatMap { config =>
+    EmberServerBuilder
+      .default[IO]
+      .withHost(config.host)
+      .withPort(config.port)
+      .withHttpApp(HealthRoutes[IO].routes.orNotFound)
+      .build
+      .use(_ => IO.println("Server ready!") *> IO.never)
+  }
 }
